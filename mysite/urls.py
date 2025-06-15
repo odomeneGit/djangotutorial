46ad1bd1b9a3b98a84bbac21da4b2760250ff8c0
vboxuser <vboxuser@vbox.home>
2025-06-15 18:53:01 +0200

    polls acabat

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from polls.views import index
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("", index), 
 ]
